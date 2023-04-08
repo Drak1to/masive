d20a7d55b6218931832dd6452e36a41f5bca6393
Drakonych <vitaliynepiyvoda@gmail.com>
2023-04-08 13:12:22 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array  = {5, 6, 8, 9, 10, 11, 22, 1};
+        for(int i = 1; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
